2878a9f30fb2ad4fd258172cb865b2cdb8b8e388
Лев Корниенко <leva.kornienko@yandex.ru>
2022-10-08 15:56:19 +0300

    Update engine.dart

--- lib/data/model/engine.dart
+++ lib/data/model/engine.dart
@@ -6,13 +6,27 @@ class Engine extends EngineEntity {
   final String engine_power;
   final String date_of_creation;
   final String engine_series;
-  Engine({required this.engine_power,required this.date_of_creation,required this.engine_series}) : super(engine_power: engine_power,date_of_creation:date_of_creation,engine_series:engine_series);
+  Engine(
+      {required this.engine_power,
+      required this.date_of_creation,
+      required this.engine_series})
+      : super(
+            engine_power: engine_power,
+            date_of_creation: date_of_creation,
+            engine_series: engine_series);
 
   Map<String, dynamic> toMap() {
-    return {'engine_power': engine_power,'date_of_creation':date_of_creation,'engine_series':engine_series};
+    return {
+      'engine_power': engine_power,
+      'date_of_creation': date_of_creation,
+      'engine_series': engine_series
+    };
   }
 
   factory Engine.toFromMap(Map<String, dynamic> json) {
-    return Engine(engine_power: json['engine_power'],date_of_creation: json['date_of_creation'],engine_series: json['engine_series']);
+    return Engine(
+        engine_power: json['engine_power'],
+        date_of_creation: json['date_of_creation'],
+        engine_series: json['engine_series']);
   }
 }
